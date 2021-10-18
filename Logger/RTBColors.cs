13713874683b2527b8ad1ba8706198769cbc0a46
USBIPAV <bill.pav@us.abb.com>
2021-10-18 13:16:42 -0400

    documented color itesm

--- Logger/RTBColors.cs
+++ Logger/RTBColors.cs
@@ -6,9 +6,15 @@ namespace PDCLogger
     [TypeConverter(typeof(ExpandableObjectConverter))]
     public class RTBColors
     {
+        /// <summary>
+        /// The background color of the Rich Text Box text
+        /// </summary>
         [Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
         public Color Background { get; set; }
 
+        /// <summary>
+        /// The foreground color of the Rich Text Box text
+        /// </summary>
         [Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
         public Color Foreground { get; set; }
     }
@@ -17,9 +23,15 @@ namespace PDCLogger
     [TypeConverter(typeof(ExpandableObjectConverter))]
     public class ConsoleColors
     {
+        /// <summary>
+        /// the background color of the Console text
+        /// </summary>
         [Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
         public System.ConsoleColor Background { get; set; }
 
+        /// <summary>
+        /// the foreground color of the Console text
+        /// </summary>
         [Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
         public System.ConsoleColor Foreground { get; set; }
     }
